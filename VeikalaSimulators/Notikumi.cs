73e8fbcdaa8097fccd30e3bc8eab0d12eb606bb5
Lewais <168599227+Lewais@users.noreply.github.com>
2024-11-26 20:13:46 +0200

    ...

--- VeikalaSimulators/Notikumi.cs
+++ VeikalaSimulators/Notikumi.cs
@@ -10,20 +10,25 @@ namespace VeikalaSimulators
     {
         public static string NotikumuGenerators(VeikalaLogika veikals, Random random)
         {
-            int NotikumaIespeja = random.Next(1, 101); 
-            if (NotikumaIespeja <= 20)
+            if (veikals.Invetars == 0)
             {
-                decimal sods = random.Next(50, 200);
-                veikals.Budzets -= sods;
-                return $"Negatīvs notikums: Klientu sūdzība! Zaudēti {penalty} €.";
+                return "Invetārā nav nevienas preces, lūdzu iegādājaties preces, lai turpinātu!";
+            } else {
+                int NotikumaIespeja = random.Next(1, 101);
+                if (NotikumaIespeja <= 20)
+                {
+                    decimal sods = random.Next(50, 200);
+                    veikals.Budzets -= sods;
+                    return $"Negatīvs notikums: Klientu sūdzība! Zaudēti {sods} €.";
+                }
+                else if (NotikumaIespeja <= 30)
+                {
+                    decimal bonuss = random.Next(50, 200);
+                    veikals.Budzets += bonuss;
+                    return $"Pozitīvs notikums: Tu saņēmi bonusu no piegādātāja! Peļņa: {bonuss} €.";
+                }
+                return string.Empty;
             }
-            else if (NotikumaIespeja <= 30) // 10% iespēja pozitīvam notikumam
-            {
-                decimal bonuss = random.Next(50, 200);
-                veikals.Budzets += bonuss;
-                return $"Pozitīvs notikums: Tu saņēmi bonusu no piegādātāja! Peļņa: {bonuss} €.";
-            }
-            return string.Empty;
         }
     }
 }
